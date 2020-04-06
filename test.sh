ac4576123080c2ca947e17a800870f3cb01eee00
Jhonatan Paul Urbina Tolentino <jhonatanurbinat@gmail.com>
2020-04-05 23:45:16 -0500

    mod tes.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl -X POST "localhost:$1/v1/depositEvent" -H 'Content-Type:application/json' --data '{"id":12345 , "amount": 123 , "type":"INFO" , "creationDate": "123" , "account": 123}'
+curl -X POST "$1:$2/v1/depositEvent" -H 'Content-Type:application/json' --data '{"id":12345 , "amount": 123 , "type":"INFO" , "creationDate": "123" , "account": 123}'
 
